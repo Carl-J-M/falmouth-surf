1f5672d8f0e8ee46a012cd4bdc642ae2efc11a3e
Carl-J-M <carlmensahmail@gmail.com>
2020-05-18 15:49:10 +0100

    Successful data fetch from provided API

--- src/containers/HomepageContainer.js
+++ src/containers/HomepageContainer.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import '../App.css';
 
-class HomepageComtainer extends React.Component {
+class HomepageContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             data: {}
         };
+        this.initalizeSpots = this.initalizeSpots.bind(this);
     }
     componentDidMount() {
         console.log("Homepage Container mounted");
+        this.initalizeSpots();
+        
+    }
+    initalizeSpots() {
+        const url =
+          "https://cors-anywhere.herokuapp.com/https://s3.eu-west-2.amazonaws.com/lpad-public-assets/software-test/all-spots.json";
+        fetch(url)
+        .then(res => res.json())
+        .then(apiData =>
+            console.log(apiData)
+        )
     }
 
     render() {
@@ -19,4 +31,4 @@ class HomepageComtainer extends React.Component {
     }
 }
 
-export default HomepageComtainer;
+export default HomepageContainer;
